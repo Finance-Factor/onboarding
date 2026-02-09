467d0496a9f12f6718a7de15f502c25e1c76c8af
maitreverge_fedora <verge.f@gmail.com>
2026-02-09 19:48:43 +0100

    BLACK linter

--- git_config.py
+++ git_config.py
@@ -6,30 +6,49 @@ COLOR_RED = "\033[91m"
 COLOR_BLINK = "\033[5m"
 COLOR_RESET = "\033[0m"
 
+
 def intro():
-    print(f"-----\n{COLOR_GREEN}{COLOR_BLINK}Welcome to the Git Configurator{COLOR_RESET}\n-----")
+    print(
+        f"-----\n{COLOR_GREEN}{COLOR_BLINK}Welcome to the Git Configurator{COLOR_RESET}\n-----"
+    )
     print("This configurator will help you configure git crucial settings.\n")
-    
+
+
 def configure_username():
-    print(f"-----\n{COLOR_GREEN}{COLOR_BLINK}GIT USERNAME CONFIGURATION{COLOR_RESET}\n-----")
-    print(f"{COLOR_RED}IMPORTANT: This username will be used to sign your commits. It is recommended to use the same username as your GitHub account.{COLOR_RESET}")
+    print(
+        f"-----\n{COLOR_GREEN}{COLOR_BLINK}GIT USERNAME CONFIGURATION{COLOR_RESET}\n-----"
+    )
+    print(
+        f"{COLOR_RED}IMPORTANT: This username will be used to sign your commits. It is recommended to use the same username as your GitHub account.{COLOR_RESET}"
+    )
     username = input("Enter the name you want to sign your commits with: ")
     subprocess.run(["git", "config", "--global", "user.name", username])
 
-    print(f"Your username has been configured as {COLOR_GREEN}{COLOR_BLINK}{username}{COLOR_RESET}")
+    print(
+        f"Your username has been configured as {COLOR_GREEN}{COLOR_BLINK}{username}{COLOR_RESET}"
+    )
+
 
 def configure_email():
-    print(f"-----\n{COLOR_GREEN}{COLOR_BLINK}GIT EMAIL CONFIGURATION{COLOR_RESET}\n-----")
-    print(f"{COLOR_RED}IMPORTANT: This email will be used to sign your commits. It must match the email associated with your GitHub account.{COLOR_RESET}")
+    print(
+        f"-----\n{COLOR_GREEN}{COLOR_BLINK}GIT EMAIL CONFIGURATION{COLOR_RESET}\n-----"
+    )
+    print(
+        f"{COLOR_RED}IMPORTANT: This email will be used to sign your commits. It must match the email associated with your GitHub account.{COLOR_RESET}"
+    )
     email = input("Enter the email you want to sign your commits with: ")
     subprocess.run(["git", "config", "--global", "user.email", email])
 
-    print(f"Your email has been configured as {COLOR_GREEN}{COLOR_BLINK}{email}{COLOR_RESET}")
+    print(
+        f"Your email has been configured as {COLOR_GREEN}{COLOR_BLINK}{email}{COLOR_RESET}"
+    )
+
 
 def main():
     intro()
     configure_username()
     configure_email()
+
 
 if __name__ == "__main__":
     main()
--- new_user_configurator.py
+++ new_user_configurator.py
@@ -4,17 +4,22 @@ import sys
 # ! This group name has been set at the begining, to allow all users to write on all files, regardless of the current file perms.
 SHARED_GROUP_NAME = "sharedgrp"
 
+
 def intro():
     print("-----\n\033[92m\033[5mWelcome to the New User Configurator\033[0m\n-----")
     # print("This configurator will help you configure git crucial settings.\n")
+
 
 def check_root():
     ss = subprocess.run(["whoami"], capture_output=True, text=True)
     username = ss.stdout.strip()
     # print(f"The current user is : {ss.stdout.strip()}")
     if username != "root":
-        print("This Python script is supposed to be run as `root`. Please login with `root` user")
+        print(
+            "This Python script is supposed to be run as `root`. Please login with `root` user"
+        )
         sys.exit(1)
+
 
 def create_new_user():
     new_username = ""
@@ -28,25 +33,31 @@ def create_new_user():
     add_sudo = ""
     while add_sudo not in ["y", "n"]:
         add_sudo = input(f"Do you want to add {new_username} in sudo group (y/n): ")
-    if add_sudo == 'y':
+    if add_sudo == "y":
         subprocess.run(["usermod", "-aG", "sudo", new_username])
 
     # Adding the new created user to Docker group
     add_docker = ""
     while add_docker not in ["y", "n"]:
-        add_docker = input(f"Do you want to add {new_username} in docker group (Highly recommanded): (y/n)")
+        add_docker = input(
+            f"Do you want to add {new_username} in docker group (Highly recommanded): (y/n)"
+        )
     if add_docker == "y":
         subprocess.run(["usermod", "-aG", "docker", new_username])
 
     # Add user to SHARED_GROUP_NAME
     subprocess.run(["usermod", "-aG", SHARED_GROUP_NAME, new_username])
 
-    print(f"-----\n\033[92m\033[5mUser {new_username} has been created, with{"out" if add_sudo == 'n' else ""} sudo perms, with{"out" if add_docker == 'n' else ""} docker perms, and it's part of the {SHARED_GROUP_NAME} group.\033[0m\n-----")
+    print(
+        f"-----\n\033[92m\033[5mUser {new_username} has been created, with{"out" if add_sudo == 'n' else ""} sudo perms, with{"out" if add_docker == 'n' else ""} docker perms, and it's part of the {SHARED_GROUP_NAME} group.\033[0m\n-----"
+    )
+
 
 def main():
     intro()
     check_root()
     create_new_user()
+
 
 if __name__ == "__main__":
     main()
