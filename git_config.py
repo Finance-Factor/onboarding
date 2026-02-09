f5dc2f27d572ce1d96b424a14b0bbbf6d137c981
maitreverge_wsl_ff <verge.f@gmail.com>
2026-02-09 15:13:52 +0100

    Fix git script blinking

--- git_config.py
+++ git_config.py
@@ -1,24 +1,30 @@
 import subprocess
 
+# ANSI color codes
+COLOR_GREEN = "\033[92m"
+COLOR_RED = "\033[91m"
+COLOR_BLINK = "\033[5m"
+COLOR_RESET = "\033[0m"
+
 def intro():
-    print("-----\nWelcome to the Git Configurator\n-----")
+    print(f"-----\n{COLOR_GREEN}{COLOR_BLINK}Welcome to the Git Configurator{COLOR_RESET}\n-----")
     print("This configurator will help you configure git crucial settings.\n")
     
 def configure_username():
-    print("-----\n\033[92m\033[5mGIT USERNAME CONFIGURATION\033[0m\n-----")
-    print("\033[91m\033[5mIMPORTANT: This username will be used to sign your commits. It is recommended to use the same username as your GitHub account.\033[0m")
+    print(f"-----\n{COLOR_GREEN}{COLOR_BLINK}GIT USERNAME CONFIGURATION{COLOR_RESET}\n-----")
+    print(f"{COLOR_RED}IMPORTANT: This username will be used to sign your commits. It is recommended to use the same username as your GitHub account.{COLOR_RESET}")
     username = input("Enter the name you want to sign your commits with: ")
     subprocess.run(["git", "config", "--global", "user.name", username])
 
-    print(f"Your username has been configured as \033[91m\033[5m{username}\033[0m")
+    print(f"Your username has been configured as {COLOR_GREEN}{COLOR_BLINK}{username}{COLOR_RESET}")
 
 def configure_email():
-    print("-----\n\033[92m\033[5mGIT EMAIL CONFIGURATION\033[0m\n-----")
-    print("\033[91m\033[5mIMPORTANT: This email will be used to sign your commits. It must match the email associated with your GitHub account.\033[0m")
+    print(f"-----\n{COLOR_GREEN}{COLOR_BLINK}GIT EMAIL CONFIGURATION{COLOR_RESET}\n-----")
+    print(f"{COLOR_RED}IMPORTANT: This email will be used to sign your commits. It must match the email associated with your GitHub account.{COLOR_RESET}")
     email = input("Enter the email you want to sign your commits with: ")
     subprocess.run(["git", "config", "--global", "user.email", email])
 
-    print(f"Your email has been configured as \033[91m\033[5m{email}\033[0m")
+    print(f"Your email has been configured as {COLOR_GREEN}{COLOR_BLINK}{email}{COLOR_RESET}")
 
 def main():
     intro()
