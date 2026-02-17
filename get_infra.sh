2609786ef4f211746b4a39254dc4a5dc1bb8f06e
maitreverge_wsl_ff <verge.f@gmail.com>
2026-02-17 14:47:00 +0100

    ADD: excluding azure

--- get_infra.sh
+++ get_infra.sh
@@ -28,7 +28,7 @@ SSH_CONFIG_FILE="$HOME/.ssh/config"
 
 sudo rsync -avz --delete \
     --exclude "secrets/staging" --exclude "secrets/prod" \
-    --exclude "node_modules" --exclude ".next" --exclude ".venv" --exclude "__pycache__" --exclude "services/appsmith" \
+    --exclude "node_modules" --exclude ".next" --exclude ".venv" --exclude "__pycache__" --exclude "services/appsmith" --exclude "/backup-azure" \
     -e "ssh -F $SSH_CONFIG_FILE" \
     "$SSH_ALIAS":/srv/shared_root/platform ~/
 
