8ed08af837907bab2e4e3a731d7c3c133de807c2
maitreverge_wsl_ff <verge.f@gmail.com>
2026-02-17 10:51:02 +0100

    Finished getting infra script

--- get_infra.sh
+++ get_infra.sh
@@ -12,16 +12,9 @@ if ! command -v rsync &> /dev/null; then
 fi
 
 # Prompt the user for the SSH alias of their main VPS session
-read -p "Enter your main VPS session SSH alias: " ssh_alias
+read -p "Enter your main VPS session personal SSH alias: " ssh_alias
 ssh_alias=$(echo "$ssh_alias" | xargs)
 SSH_ALIAS="$ssh_alias"
-
-# Prompt the user for the destination folder name
-# read -p "Enter the destination folder name in your home (Just hit ENTER for default name: platform): " folder_name
-# folder_name=${folder_name:-platform}
-# FOLDER_NAME="$folder_name"
-
-# mkdir -p ~/"$FOLDER_NAME"
 
 # Check if the SSH alias is defined in the SSH config file
 SSH_CONFIG_FILE="$HOME/.ssh/config"
